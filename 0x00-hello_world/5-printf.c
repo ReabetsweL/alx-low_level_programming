4af44558c150017a63c1943f2f32c9807fd6972d
ReabetsweL <Reabetswellale@gmail.com>
2023-02-17 07:37:43 -0800

    task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-#include<stadio.h>
+#include<stdio.h>
 /**
- * main -entry of program
+ * main -entry point of program
  *
- * Return: zero if no errors, non-zero if error
+ * Return: zero if no erros, non-zero if error
  */
 int main(void)
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
