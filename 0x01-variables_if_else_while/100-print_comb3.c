f4f7b48803af5e1ed3db33e5a00a5b51d37d4e95
ReabetsweL <Reabetswellale@gmail.com>
2023-02-18 12:41:45 -0800

    task 6

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,44 @@
+/*
+ * File: 100-print_comb3.c
+ * Auth: ReabetsweL
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+
+
+int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
