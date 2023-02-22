00c6e3320b68f50a0c165f6d1a5b8f7a80026ac6
ReabetsweL <Reabetswellale@gmail.com>
2023-02-22 04:15:12 -0800

    task 8

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,12 @@
-#include "main.h"
+#include <unistd.h>
 
 int main() {
-    printf("_putchar\n");
+    char str[] = "_putchar\n";
+    int len = sizeof(str) / sizeof(char);
+
+    for (int i = 0; i < len; i++) {
+        _putchar(str[i]);
+    }
+
     return 0;
 }
