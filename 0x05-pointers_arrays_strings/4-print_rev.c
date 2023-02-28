71a91c3c4edba3c02f574a644c821311d2e94831
ReabetsweL <Reabetswellale@gmail.com>
2023-02-28 03:10:37 -0800

    task 6

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include "main.h"
 
 /**
  * print_rev - print a string in reverse.
@@ -16,7 +16,7 @@ void print_rev(char *s)
 	{
 	}
 
-	for (i = length - 1; i >= 0; i --)
+	for (i = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
