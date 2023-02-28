75d9e259bc8893944585830620132ec77e4c95a4
ReabetsweL <Reabetswellale@gmail.com>
2023-02-28 04:38:37 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+/*
+ * File: 6-puts2.c
+ * Auth: ReabetsweL
+ */
+
+#include "main.h"
+
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index +=2)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
