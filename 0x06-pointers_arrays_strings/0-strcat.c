c57e035548e660439e39c865daad3092e0f2ebcb
ReabetsweL <Reabetswellale@gmail.com>
2023-03-02 00:38:02 -0800

    task 6

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,6 +6,7 @@
 #include "main.h"
 
 /**
+ *
  * _strcat - Concatenates two strings.
  *
  * @dest: Pointer to the destination string.
@@ -28,6 +29,7 @@ char *_strcat(char *dest, char *src)
         j++;
     }
 
-    dest[i] = '\0';
+    dest[i] = '\n'; /* Add a new line character */
+    dest[i + 1] = '\0'; /* Add the terminating null byte */
     return (dest);
 }
