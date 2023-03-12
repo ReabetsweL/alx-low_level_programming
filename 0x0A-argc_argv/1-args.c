6272497746abf20947203ced9ef2cc9ed19b3e41
ReabetsweL <Reabetswellale@gmail.com>
2023-03-12 16:52:43 -0700

    task 6

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,14 +2,14 @@
 
 /**
  * main - Prints the number of arguments passed to it.
- * @argc: The number of arguments apssed to it. 
+ * @argc: The number of arguments apssed to it.
  * @argv: An array of pointers to the arguments.
- * 
+ *
  * Return: Always 0.
  */
 int main(__attribute__((unused)) *argv[])
 {
-	printf("%d\n", argc -1);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
