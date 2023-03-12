f9374b8e3f0ed35fd3868d5721c3bfe035eb838e
ReabetsweL <Reabetswellale@gmail.com>
2023-03-12 16:38:56 -0700

    task 6

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name followed by a new line.
+ * @argc: The number of arguments in the program.
+ * @argv: The array of arguments. 
+ *
+ * Return: Always 0.
+ */
+int main(__attribute__((unused)) argc, char*argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
