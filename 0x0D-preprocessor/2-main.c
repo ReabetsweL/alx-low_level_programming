823f2815b6c1b2b716cd489d74dd1e1788aa3721
ReabetsweL <Reabetswellale@gmail.com>
2023-03-20 15:11:09 -0700

    task 6

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib>
+
+/*
+ * main - prints the name of the file.
+ * Return: Always return 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", Preprocessor);
+
+	return (0);
+}
