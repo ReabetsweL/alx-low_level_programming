f8960866783c5e54988f7ee72e2cb90dc0c8fbff
ReabetsweL <Reabetswellale@gmail.com>
2023-03-20 15:47:16 -0700

    task 6

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define main
 
-/*
- * main - Entry point
+/* main - Entry point
  * Description: Prints the name of the file that it was compiled from.
  * Return: Always 0 (Success)
  */
