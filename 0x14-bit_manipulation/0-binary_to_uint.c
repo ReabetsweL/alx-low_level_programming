cbe51f2c7c804fea89543647bbb070b59f3d829d
ReabetsweL <Reabetswellale@gmail.com>
2023-04-07 15:03:10 -0700

    task 6

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int dec = 0;
+	unsigned int dec_val = 0;
 
 	if (!b)
 		return (0);
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec = 2 * dec + (b[i] - '0');
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (dec)
+	return (dec_val);
 }
