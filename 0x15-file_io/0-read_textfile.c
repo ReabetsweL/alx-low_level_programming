acab0a1768c4cfe988db2ec40202a28683e63c31
ReabetsweL <Reabetswellale@gmail.com>
2023-04-10 00:37:32 -0700

    task 6

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,31 +8,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i, y;
-	char *buf;
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+
+	int fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
+
+	char *buf = malloc(sizeof(char) * letters);
+
+	if (buf == NUL)
 	{
-		free(buf);
+		close(fd);
 		return (0);
 	}
-	buf[i] = '\0';
-	close (fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
+	ssize_t bytes_writen = write(STDOUT_FILENO, buf, bytes_read);
+
+	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
 		free(buf);
+		close(fd);
 		return (0);
 	}
-	free(buff);
-	return (y);
+	free(buf);
+	close(fd);
+	return (bytes_written);
 }
